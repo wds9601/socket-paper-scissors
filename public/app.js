8efa27ee2388c1f75fe91df7901f66adbe424800
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-01-02 15:53:56 -0800

    added choice selection for game

--- public/app.js
+++ public/app.js
@@ -5,14 +5,15 @@ let btn
 let choices = []
 let game
 let login
+let submitted = false
 let username
 
 document.addEventListener('DOMContentLoaded', () => {
     btn = document.getElementById('btn').addEventListener('click', submitLogin)
     choices = document.querySelectorAll('.game-choice')
-    // choices.forEach(choices => {
-    //     choices.addEventListener('click', submitChoice)
-    // })
+    choices.forEach(choices => {
+        choices.addEventListener('click', submitChoice)
+    })
     game = document.getElementById('game')
     login = document.getElementById('login')
     username = document.getElementById('username')
@@ -27,6 +28,17 @@ const submitLogin = () => {
             alert(`${username.value} is already in use!`)
         }
     })
+}
+
+const submitChoice = e => {
+    let choice = e.target.id
+    console.log(username.value, choice)
+    if (!submitted) {
+        submitted = true
+        socket.emit('player choice', username.value, choice)
+    } else {
+        console.log('You have already made your choice!')
+    }
 }
 
 socket.on('get players', data => {
